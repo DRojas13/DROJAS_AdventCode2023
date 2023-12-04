6cc7812305f3287726b32f642f3aff8bbad63729
Daniel Rojas Jr <danny.rojas.jr@gmail.com>
2023-12-04 13:15:38 -0500

    Day 1 - Solved

--- Advent2023_Day1.py
+++ Advent2023_Day1.py
@@ -14,6 +14,7 @@
 
 # Variable collective
 calibration_values = list() # Target list for all of my extracted values
+part2_calibration_values = list() # Target list for extracted values for part 2
 
 
 data_day_1 = open("Data_Files\Day1.txt", "r") # Opens the data file
@@ -38,6 +39,43 @@ def calibration_extractor(line_of_text):
         
     # Add the two digit number to the global list
     calibration_values.append(int(combined_value))
+
+
+def part2_calibration_extractor(line_of_text):
+    """
+    Function used to re-evaluate the data file text for part 2 of the puzzle.
+    The new parameters include words for numbers amongst the number values.
+    """
+    text_number_list = ['one','two','three','four','five','six','seven','eight','nine']
+    identified_numbers = dict()
+    index_start = -1
+    
+
+    for index, value in enumerate(line_of_text):
+        if value.isdigit() == True:
+            identified_numbers[index] = value
+
+    # Checking the word versions of the numbers in the strings
+    for number in text_number_list:
+        testing = number in line_of_text
+        if testing == True:
+            # check for each instance of the found word and its index
+            while True:
+                index_start = line_of_text.find(number, index_start + 1)
+                if index_start == -1:
+                    break
+                else:
+                    identified_numbers[index_start] = str(text_number_list.index(number) + 1)
+    
+    # Take the newly created dictionary and sort based on the key        
+    sortedList = dict(sorted(identified_numbers.items()))
+    # Create a list of the values from the sorted dictionary
+    values_only = list(sortedList.values())
+    # Create the combined value based on the sorted list
+    combined_value = values_only[0] + values_only[-1]
+    # Add the two digit number to the global list
+    part2_calibration_values.append(int(combined_value))
+    # input()
     
             
 def total_of_calibration_values(values_list, part):
@@ -66,8 +104,11 @@ def main():
     total_of_calibration_values(calibration_values, 'ONE')
     
     # Part 2 is addressed here
+    for part_2_content in data:
+        part2_calibration_extractor(part_2_content)
 
-    total_of_calibration_values(calibration_values, 'ONE')
+    
+    total_of_calibration_values(part2_calibration_values, 'TWO')
 
     
 
